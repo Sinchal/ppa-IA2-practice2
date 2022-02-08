2e9e4d3a04fa467e8f1c7127ada26fa8745145e4
Sinchal <94883181+Sinchal@users.noreply.github.com>
2022-02-08 04:36:29 +0000

    done

--- p2original.c
+++ p2original.c
@@ -1,20 +1,38 @@
 #include<stdio.h>
 
-int input_side()
+void input_side(int *a, int *b, int *c)
 {
-  int a, b, c;
-  printf("enter sides of triangle");
-  scanf("%d%d%d",&a,&b,&c);
-  return 
+  printf("enter sides of triangle\n");
+  scanf("%d%d%d",a,b,c);
 }
 
 int check_scalene(int a, int b, int c)
 {
-  int isscalene;
-  if (a==b && b==c && a==c)
-  {
-    isscalene=1;
-  }
+ int isscalene;
+ if (a!=b && b!=c && a!=c)
+ {
+   isscalene=1;
+ }
  return isscalene;  
 }
 
+void output(int a, int b, int c, int isscalene)
+{
+ if (isscalene==1)
+ {
+   printf("triangle is scalene\n");
+ }
+ else
+ {
+   printf("the triangle is equilateral or isoceles\n");
+ }
+}
+
+int main()
+{
+ int a,b,c,isscalene;
+ input_side(&a,&b,&c);
+ isscalene=check_scalene(a,b,c);
+ output(a,b,c,isscalene);
+ return 0;
+}
